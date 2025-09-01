88e3b1a1413f05b6da0639b8b65a1a87945ac040
ibrahim300-data <ibrahimnaim300@gmail.com>
2025-09-01 21:28:48 +0300

    grass.py

--- grass.py
+++ grass.py
@@ -6,7 +6,13 @@ def reinject_data(original_file, modified_file, offset_hex, output_file):
     with open(original_file, "rb") as f:
         original_data = bytearray(f.read())
 
-    
+
+def reinject_data(original_file, modified_file, offset_hex, output_file):
+    offset = int(offset_hex, 16)
+
+    with open(original_file, "rb") as f:
+        original_data = bytearray(f.read())
+
 def reinject_data(original_file, modified_file, offset_hex, output_file):
     offset = int(offset_hex, 16)
 
